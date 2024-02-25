2fe8e1a6ff58dd13b3084715c010518c5a2c51d5
NULL-HEX-ADI <160856923+NULL-HEX-ADI@users.noreply.github.com>
2024-02-26 00:29:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "23961203"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "0bd4802aef5d02de2c090197c719e202" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "7100747820:AAF_DgK2mGG0WJb5EQ0Ec00ob3Z1cRmE5BQ" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
